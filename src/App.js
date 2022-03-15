fee0d4ca93e22c3cfad176f45006a47c016939eb
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-03-15 13:23:25 -0500

    functional component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Modules from "./Components/modules";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>JavaScript ES6 Modules</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link to repo
         </a>
+        <Modules />
       </header>
     </div>
   );
---
+++ src/Components/modules.js
@@ -0,0 +1,14 @@
+function Modules() {
+  return (
+    <div>
+      <h2>ES6 Modules</h2>
+      <p>
+        JavaScript modules allow you to break up your code into separate files.
+        This makes it easier to maintain the code-base. ES Modules rely on the
+        import and export statements.
+      </p>
+    </div>
+  );
+}
+
+export default Modules;
